ecb9a7611cbd6d0927f4c214178e606ad67020e7
Mitch Toran <mitchellht34@gmail.com>
2024-05-21 15:09:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let fare = distanceTravelledInFeet(start, destination);
+    if(fare <= 400){
+        return 0;    
+    }
+    else if(fare <= 2000){
+        return (fare - 400) * 0.02;
+    }
+    else if(fare <= 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
